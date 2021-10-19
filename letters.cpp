72f35a00e4071d1ac1694d58bb3cc9c13e226f2b
Злата <zlatatokarenko17@yandex.ru>
2021-10-19 09:54:12 +0300

    added l

--- letters.cpp
+++ letters.cpp
@@ -55,7 +55,12 @@ Letter& alphabet::upperK() {
 }
 
 Letter& alphabet::upperL() {
-    return letter << "L";
+    return letter
+    << "||    "
+    << "||    "
+    << "||    "
+    << "||    "
+    << "||____";
 }
 
 Letter& alphabet::upperM() {
