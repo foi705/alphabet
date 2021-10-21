afd8e6830829774ab2c94290c2121ac03278434b
foi705 <foi.reff@gmail.com>
2021-10-21 12:02:56 +0300

    fixed m

--- letters.cpp
+++ letters.cpp
@@ -53,7 +53,7 @@ Letter& alphabet::upperH() {
 }
 
 Letter& alphabet::upperI() {
-    return letter << "I";
+    return letter
     << "-----"
     << "  |  "
     << "  |  "
@@ -80,9 +80,9 @@ Letter& alphabet::upperL() {
 
 Letter& alphabet::upperM() {
     return letter
-    << "|\    /|"
-    << "| \  / |"
-    << "|  \/  |"
+    << "|\\    /|"
+    << "| \\  / |"
+    << "|  \\/  |"
     << "|      |"
     << "|      |";
 }
